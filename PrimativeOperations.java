a8a3c4419865632b4f881ac6d8098eb9992d35c5
WangrudCodes <116783763+WangrudCodes@users.noreply.github.com>
2022-10-26 23:43:07 -0700

    Part 1 halfway done

---
+++ PrimativeOperations.java
@@ -0,0 +1,19 @@
+public class PrimativeOperations{
+
+public static void main(String[] args){
+//Declare an int variable
+int int1 = 4;
+//Declare a float variable
+float float1 = 6.7;
+
+System.out.println(int1);
+System.out.println(float1);
+
+double multiply =int1*float1;
+System.out.println(multiply);
+
+}
+
+
+
+}
