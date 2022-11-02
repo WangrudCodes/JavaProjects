33359a16f32a6188669bbaf929ffc535b4c91392
drizz <drizztsmail@gmail.com>
2022-11-01 20:52:53 -0700

    Using methods in other files practice

---
+++ Strings and Arrays.java/SomeOtherProgram.java
@@ -0,0 +1,12 @@
+public class SomeOtherProgram {
+    public static void main(String args[]) {
+        String[] check = {"hello", "world", "!"};
+        int[] chic = {0,1,5,9,7};
+            System.out.println(ArraySearch.searchArray("hey",check));
+            System.out.println(ArraySearch.searchArray(1,chic));
+
+
+
+
+    }
+}
