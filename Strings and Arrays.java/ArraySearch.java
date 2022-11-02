d6507c15bb75e14b3fed7cd6b5bba3965346e73a
drizz <drizztsmail@gmail.com>
2022-11-01 20:24:16 -0700

    Array Search method creation

--- Strings and Arrays.java/ArraySearch.java
+++ Strings and Arrays.java/ArraySearch.java
@@ -1,3 +1,25 @@
 public class ArraySearch {
-    
+    public static void main(String[] args){
+        String[] concepts = {"abstraction",
+                            "polymorphism",
+                            "inheritance",
+                            "encapsulation"
+        };
+        String result = searchStringArray("abstraction", concepts);
+
+        System.out.println(result);
+
+
+        
+    }
+    public static String searchStringArray(String target, String[] array){
+        String result ="not found";
+        for (String targets : array){
+            if(targets.equals(target)){
+                result="found";
+                break;
+            }
+        }
+        return result;
+    }
 }
